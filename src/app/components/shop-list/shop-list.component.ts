3b5d49194aca60825608b24fd08df956f587f04e
Amonratchantawon <amonrat.cha@northbkk.ac.th>
2019-01-24 11:41:35 +0700

    shop list

--- src/app/components/shop-list/shop-list.component.ts
+++ src/app/components/shop-list/shop-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-shop-list',
@@ -7,28 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopListComponent implements OnInit {
 
-  constructor() { }
+  @Input() data: any;
+  @Output() shopdatalist: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor() {
+    // console.log(this.data)
+  }
+  ngOnInit() {
+    // console.log(this.data)
+  }
 
   shopdetail = [
     {
-      name:"Shop Boom",
-      image:"https://upload.wikimedia.org/wikipedia/commons/6/60/Food_Shop_on_Electric_Avenue_-_geograph.org.uk_-_430378.jpg"
+      name: "Shop Boom",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Food_Shop_on_Electric_Avenue_-_geograph.org.uk_-_430378.jpg"
     },
     {
-      name:"Shop Pure",
-      image:"https://upload.wikimedia.org/wikipedia/commons/6/60/Food_Shop_on_Electric_Avenue_-_geograph.org.uk_-_430378.jpg"
+      name: "Shop Pure",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Food_Shop_on_Electric_Avenue_-_geograph.org.uk_-_430378.jpg"
     },
     {
-      name:"Shop Nid Nid",
-      image:"https://upload.wikimedia.org/wikipedia/commons/6/60/Food_Shop_on_Electric_Avenue_-_geograph.org.uk_-_430378.jpg"
+      name: "Shop Nid Nid",
+      image: "https://upload.wikimedia.org/wikipedia/commons/6/60/Food_Shop_on_Electric_Avenue_-_geograph.org.uk_-_430378.jpg"
     },
     {
-      name:"Shop Nut Shop Nut Shop Nut",
-      image:"http://www.lepfitness.co.uk/wp-content/uploads/2017/02/Shopping-basket.jpg"
+      name: "Shop Nut Shop Nut Shop Nut",
+      image: "http://www.lepfitness.co.uk/wp-content/uploads/2017/02/Shopping-basket.jpg"
     }
   ]
-
-  ngOnInit() {
-  }
-
 }
